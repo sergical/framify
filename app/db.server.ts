e8989426f4879086a03a2fc26c010ec823d63c0b
Sergiy Dybskiy <s@serg.tech>
2024-03-22 13:09:28 -0400

    fix db

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,26 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient;
 }
 
-let prisma: PrismaClient;
+const prisma: PrismaClient = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-  // Use the production schema file
-  prisma =
-    global.prisma ||
-    new PrismaClient({
-      datasources: { db: { url: process.env.DATABASE_URL } },
-    });
-} else {
-  // Use the development schema and ensure only one instance is used in development
+if (process.env.NODE_ENV !== "production") {
   if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      datasources: { db: { url: "file:./dev.sqlite" } },
-    });
+    global.prisma = new PrismaClient();
   }
-  prisma = global.prisma;
 }
 
 export default prisma;
